f3721de3972cc1d4751a80e82a25638b365c6845
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-07-12 15:41:40 +0600

    add taask details in models

--- tasks/models.py
+++ tasks/models.py
@@ -7,18 +7,14 @@ class Employee(models.Model):
 
     def __str__(self):
         return self.name
- 
+
+
 # Create your models here.
-class Task(models.Model): # delete the project if the task is deleted
-    project=models.ForeignKey(
-        "Project",
-        on_delete=models.CASCADE,
-        default=1
-        
-        ) 
-    
-    assigned_to=models.ManyToManyField(Employee,related_name='tasks')
-    
+class Task(models.Model):
+    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1) # One to many relationship (child=task... parent=project)
+
+    assigned_to = models.ManyToManyField(Employee, related_name="tasks") # many to many field
+
     # new_string=models.CharField(max_length=250)
     title = models.CharField(max_length=250)
     description = models.TextField()
@@ -27,32 +23,29 @@ class Task(models.Model): # delete the project if the task is deleted
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-#Task.object.get(id=2)
-#ORM
-#One to One
-#One to many
-#Many to many
+
+# Task.object.get(id=2)
+# ORM
+# One to One
+# One to many
+# Many to many
+
 
 class TaskDetail(models.Model):
-    High='H'
-    Medium='M'
-    LOW='L'
+    High = "H"
+    Medium = "M"
+    LOW = "L"
     PRIORITY_OPTIONS = (
-        (High,'High'),
-        (Medium,'Medium'),
-        (LOW,'Low'),
+        (High, "High"),
+        (Medium, "Medium"),
+        (LOW, "Low"),
     )
-    task=models.OneToOneField(
-        Task,
-        on_delete=models.CASCADE,
-        related_name='details'
-        
-        ) # one to one relationship
-    assigned_to=models.CharField(max_length=100)
-    priority=models.CharField(
-        max_length=1, choices=PRIORITY_OPTIONS,default=LOW
-    )
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name="details")
+    # one to one relationship
+    assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+
 
 class Project(models.Model):
-    name=models.CharField(max_length=100)
-    start_date=models.DateField()
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
