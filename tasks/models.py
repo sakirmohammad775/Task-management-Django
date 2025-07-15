f83aec8b61d24edbc1e442a477092e0acbded566
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-07-15 22:59:48 +0600

    add task form

--- tasks/models.py
+++ tasks/models.py
@@ -5,23 +5,36 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
 
-    def __str__(self): # pragma: no cover
-        return self.name 
+    def __str__(self):  # pragma: no cover
+        return self.name
 
 
 # Create your models here.
 class Task(models.Model):
-    project = models.ForeignKey("Project", on_delete=models.CASCADE, default=1) # One to many relationship (child=task... parent=project)
+    STATUS_CHOICES = [
+        ("PENDING", "pending"),
+        ("IN_PROGRESS", "in progress"),
+        ("COMPLETED", "completed"),
+    ]
+    project = models.ForeignKey(
+        "Project", on_delete=models.CASCADE, default=1
+    )  # One to many relationship (child=task... parent=project)
 
-    assigned_to = models.ManyToManyField(Employee, related_name="tasks") # many to many field (one employee can have many tasks) and one task can be assigned to many employees
+    assigned_to = models.ManyToManyField(
+        Employee, related_name="tasks"
+    )  # many to many field (one employee can have many tasks) and one task can be assigned to many employees
 
     # new_string=models.CharField(max_length=250)
     title = models.CharField(max_length=250)
     description = models.TextField()
     due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 # Task.object.get(id=2)
@@ -40,12 +53,20 @@ class TaskDetail(models.Model):
         (Medium, "Medium"),
         (LOW, "Low"),
     )
+    # std_id=models.CharField( max_length=100, unique=True)
     task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name="details")
     # one to one relationship
     assigned_to = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f"Details from Task{self.task.title}"
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
